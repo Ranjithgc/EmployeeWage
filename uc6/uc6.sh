6dfb5b57159bd56f287427d70943a1a658b839c2
Ranjithgc <ranjith.amca.16@acharya.ac.in>
2021-04-29 17:55:17 +0530

    Add: uc6 Programs

---
+++ uc6/uc6.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x
+echo "Welcome to Employee Wage Computation Program on Master Branch"
+isFullTime=1
+isAbsent=0
+isPartTime=2
+FullTimeHr=8
+PartTimeHr=4
+SalaryPerHr=20
+totalSalary=0
+monthlySalary=0
+attendance=$((RANDOM%3))
+
+case $attendance in
+        1)
+        echo "Employee is Present"
+                ;;
+        2)
+        echo "Employee is Part Timer"
+                ;;
+        *)
+        echo "Employee is absent"
+                ;;
+esac
+case $attendance in
+        1)
+        totalSalary=$(($totalSalary+($SalaryPerHr*$FullTimeHr)))
+        monthlySalary=$(($totalSalary*20))
+        echo "Monthly Salary: $monthlySalary"
+                ;;
+        2)
+        totalSalary=$(($totalSalary+($SalaryPerHr*$PartTimeHr)))
+                ;;
+        *)
+        totalSalary=0
+                ;;
+esac
+echo "Salary is: $totalSalary"
